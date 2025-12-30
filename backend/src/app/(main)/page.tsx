9dc3ec3717f9999a07f7866e44f216d328bbc654
DavePHP32 <daviddembele26@yahoo.com>
2025-12-30 13:46:30 +0000

    new project's structuration

---
+++ backend/src/app/(main)/page.tsx
@@ -0,0 +1,7 @@
+
+export default function Home() {
+  return (
+    <div className="flex min-h-full bg-zinc-50 max-h-full items-center justify-center dark:bg-black ">
+    </div>
+  );
+}
---
+++ backend/src/app/(main)/users/page.tsx
@@ -0,0 +1,7 @@
+import ListUsersPage from "@/src/frontend/users/list-users/list-users.page";
+
+
+export default function page() {
+    
+    return <ListUsersPage />
+}
