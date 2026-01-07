50972c991b5b2aff34f8829bffe9067568d548e8
Abdoulaye <abdoulaye.cisse@samina.tech>
2026-01-07 12:15:00 +0000

    Code clean dans gestionnaire

--- src/app/(main)/gestionnaire/page.tsx
+++ src/app/(main)/gestionnaire/page.tsx
@@ -14,6 +14,11 @@ import { Span } from "next/dist/trace";
 import { VehicleFilters } from "./shared/components/vehicle-filters";
 import { VehicleListAll } from "./shared/components/vehicle-list-all";
 import { VehicleListByAgence } from "./shared/components/vehicle-list-by-agence";
+import { VehicleSearchBar } from "./shared/components/vehicle-search-bar";
+import { VehicleStats } from "./shared/components/vehicule-stats";
+import { VehicleListCard } from "./shared/components/vehicle-list-card";
+import { VehicleNotFound } from "./vehicle-not-found";
+import { VehiclePreview } from "./shared/components/vehicle-apercu";
 
 export default function GestionnairePage(){
 
@@ -140,12 +145,8 @@ export default function GestionnairePage(){
         <div className="h-full py-4 px-12 bg-zinc-50">
             <div className="bg-white min-h-full rounded-lg p-4">
                 <span className="font-bold text-2xl">Page Gestionnaire</span>
-                <div className="flex p-4 items-center justify-center mt-4 shadow rounded ">
-                    <div className="flex items-center justify-center gap-2 w-full md:w-[80%] flex-col md:flex-row mx-auto">
-                        <Input className="md:flex-1"/>
-                        <Button onClick={()=>setVehiculeNotFound(true)} className="w-full md:w-auto">Rechercher</Button>
-                    </div>
-                </div>
+                <VehicleSearchBar onSearch={() => setVehiculeNotFound(true)} />
+
                 { !vehiculeNotFound ?
                     <div>
                         <div className="flex items-center gap-2 py-3">
@@ -153,124 +154,19 @@ export default function GestionnairePage(){
                             <Button variant={filterByAllVehicule ? "outline" : "default"} onClick={()=>setFilterByAllVehicule(false)}>Par Agence</Button>
                         </div>
                         <VehicleFilters agences={mockAgences} clients={mockClients} />
-                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-6">
-                            
-                            <div className="min-h-[150px] bg-blue-50 border border-blue-200 shadow rounded p-2 flex flex-col">
-                                <div className="flex flex-col flex-1">
-                                <div className="flex gap-3 items-center">
-                                    <span><CarFront color="blue" size={25} /></span>
-                                    <span className="font-bold text-lg">Total</span>
-                                </div>
-                                <span className="text-center w-full mt-2 text-4xl font-bold text-blue-600">10</span>
-                                </div>
-                                <span className="text-gray-600 text-sm">Vehicule totale</span>
-                            </div>
-
-                            <div className="min-h-[150px] bg-orange-50 border border-orange-200 shadow rounded p-2 flex flex-col">
-                                <div className="flex flex-col flex-1">
-                                <div className="flex gap-3 items-center">
-                                    <span><Loader color="orange" size={25} /></span>
-                                    <span className="font-bold text-lg">En cours</span>
-                                </div>
-                                <span className="text-center w-full mt-2 text-4xl font-bold text-orange-500">23</span>
-                                </div>
-                                <span className="text-gray-600 text-sm">Vehicule en cours</span>
-                            </div>
-
-                            <div className="min-h-[150px] bg-green-50 border border-green-200 shadow rounded p-2 flex flex-col">
-                                <div className="flex flex-col flex-1">
-                                <div className="flex gap-3 items-center">
-                                    <span><CheckCircle color="green" size={25} /></span>
-                                    <span className="font-bold text-lg">Terminé</span>
-                                </div>
-                                <span className="text-center w-full mt-2 text-4xl font-bold text-green-600">15</span>
-                                </div>
-                                <span className="text-gray-600 text-sm">Vehicule terminé</span>
-                            </div>
-
-                            <div className="min-h-[150px] bg-red-50 border border-red-200 shadow rounded p-2 flex flex-col">
-                                <div className="flex flex-col flex-1">
-                                <div className="flex gap-3 items-center">
-                                    <span><AlertCircle color="red" size={25} /></span>
-                                    <span className="font-bold text-lg">Sans int</span>
-                                </div>
-                                <span className="text-center w-full mt-2 text-4xl font-bold text-red-600">8</span>
-                                </div>
-                                <span className="text-gray-600 text-sm">Vehicule sans intervention</span>
-                            </div>
-
-                        </div>
-                        <div className="mt-6 border p-3 rounded ">
-                            <div className="flex items-center justify-between">
-                                <div className="flex items-center gap-2 font-bold">
-                                    <List  size={25} />
-                                    <h1>Liste des vehicules</h1>
-                                </div>
-                                <div className="flex items-center gap-4 hidden lg:inline">
-                                    <div className="flex items-center gap-1 text-sm tetx-gray-300">
-                                        <span className="bg-yellow-400 rounded-full w-4 h-4 inline-block"></span>
-                                        <span>En réparation</span>
-                                    </div>
-                                    <div className="flex items-center gap-1 text-sm tetx-gray-300">
-                                        <span className="bg-green-400 rounded-full w-4 h-4 inline-block"></span>
-                                        <span>Terminé</span>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="max-h-[500px] overflow-auto mt-4 shadow">
-                                {filterByAllVehicule ? (
-                                    <VehicleListAll
-                                    vehicles={mockVehicules}
-                                    clients={mockClients}
-                                    agences={mockAgences}
-                                    onSelect={() => setApercuVehiculeOpen(true)}
-                                    />
-                                ) : (
-                                    <VehicleListByAgence
-                                    vehicles={mockVehicules}
-                                    agences={mockAgences}
-                                    clients={mockClients}
-                                    onSelect={() => setApercuVehiculeOpen(true)}
-                                    />
-                                )}
-                            </div>
-                        </div>
+                        <VehicleStats total={80} enCours={23} termine={15} sansIntervention={8} />
+                        <VehicleListCard
+                            filterByAllVehicule={filterByAllVehicule}
+                            vehicles={mockVehicules}
+                            clients={mockClients}
+                            agences={mockAgences}
+                            onSelect={() => setApercuVehiculeOpen(true)}
+                        />
                     </div> :
-                    <div className="w-full lg:w-[70%] mt-14 mx-auto rounded-2xl border bg-white/70 backdrop-blur 
-                        shadow-lg p-16 flex flex-col items-center gap-6">
-
-                        <div className="w-20 h-20 flex items-center justify-center rounded-full bg-red-50 text-red-500">
-                            <SearchX size={42} />
-                        </div>
-
-                        <h1 className="text-2xl font-semibold text-zinc-800">
-                            Véhicule introuvable
-                        </h1>
-
-                        <p className="text-zinc-500 text-center max-w-md">
-                            Aucun véhicule ne correspond à votre recherche.
-                        </p>
-
-                        <div className="flex flex-col lg:flex-row gap-4 mt-4">
-                            <Button
-                                variant="outline"
-                                className="flex items-center gap-2"
-                                onClick={() => setVehiculeNotFound(false)}
-                            >
-                            <ArrowLeft size={18} />
-                                Revenir à la recherche
-                            </Button>
-
-                            <Button
-                                className="flex items-center gap-2"
-                                onClick={() => setOpenCreateVehiculeModal(true)}
-                            >
-                            <PlusCircle size={18} />
-                                Créer un nouveau véhicule
-                            </Button>
-                        </div>
-                    </div>
+                     <VehicleNotFound
+                        onBack={() => setVehiculeNotFound(false)}
+                        onCreate={() => setOpenCreateVehiculeModal(true)}
+                    />
                 }
             </div> 
 
@@ -283,73 +179,18 @@ export default function GestionnairePage(){
             <Modal open={apercuVehiculeOpen} onClose={()=>setApercuVehiculeOpen(false)} modalTitle="Aperçu" >
                 <div className="space-y-6 max-h-[500px] overflow-auto">
 
-                    <div className="rounded-xl border bg-gradient-to-r from-zinc-50 to-white p-5 shadow-sm">
-                        <div className="flex items-center gap-3">
-                            <div className="h-12 w-12 rounded-full bg-blue-100 text-blue-600 flex items-center justify-center">
-                            <Car size={22} />
-                            </div>
-
-                            <div className="flex flex-col">
-                            <span className="text-lg font-semibold text-zinc-800">
-                                DDDF
-                            </span>
-                            <span className="text-sm text-zinc-500">
-                                Citroën Megane • 2025
-                            </span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-                        <div className="flex items-center gap-3 rounded-lg border p-4">
-                            <User className="text-zinc-400" size={18} />
-                            <div>
-                            <p className="text-xs text-zinc-400">Client</p>
-                            <p className="font-medium text-zinc-700">Dave DI</p>
-                            </div>
-                        </div>
-
-                        <div className="flex items-center gap-3 rounded-lg border p-4">
-                            <MapPin className="text-zinc-400" size={18} />
-                            <div>
-                            <p className="text-xs text-zinc-400">Agence</p>
-                            <p className="font-medium text-zinc-700">Charles de Gaulle</p>
-                            </div>
-                        </div>
-
-                        <div className="flex items-center gap-3 rounded-lg border p-4">
-                            <Calendar className="text-zinc-400" size={18} />
-                            <div>
-                            <p className="text-xs text-zinc-400">Date d’entrée</p>
-                            <p className="font-medium text-zinc-700">17/12/2025</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="flex flex-col sm:flex-row gap-3">
-                        <div className="flex-1 rounded-lg bg-yellow-50 border border-yellow-200 p-4 flex items-center gap-3">
-                            <Wrench className="text-yellow-600" />
-                            <div>
-                            <p className="text-xs text-yellow-700">En réparation</p>
-                            <p className="text-xl font-bold text-yellow-800">1</p>
-                            </div>
-                        </div>
-
-                        <div className="flex-1 rounded-lg bg-green-50 border border-green-200 p-4 flex items-center gap-3">
-                            <CheckCircle2 className="text-green-600" />
-                            <div>
-                            <p className="text-xs text-green-700">Terminé</p>
-                            <p className="text-xl font-bold text-green-800">1</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="flex justify-center pt-2">
-                        <Button className="flex items-center gap-2" >
-                            <PlusCircle size={18} />
-                            Nouvelle intervention
-                        </Button>
-                    </div>
+                    <VehiclePreview
+                        licensePlate="DDDF"
+                        brand="Citroën"
+                        model="Megane"
+                        year={2025}
+                        client="Dave DI"
+                        agence="Charles de Gaulle"
+                        entreeDate="17/12/2025"
+                        enReparation={1}
+                        termine={1}
+                        onNewIntervention={() => console.log("Nouvelle intervention")}
+                    />
 
                 </div>
             </Modal>
