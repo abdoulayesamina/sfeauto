89c6ca66eb4c4c8e1d93a9ab8bb26a36d0e49d08
Abdoulaye <abdoulaye.cisse@samina.tech>
2026-01-21 16:08:41 +0000

    Page famille, collections, article done.

---
+++ src/app/(main)/famille/forms/famille-form.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { Button } from "@/src/shared/components/ui/button"
+import { Input } from "@/src/shared/components/ui/input"
+import { Label } from "@/src/shared/components/ui/label"
+
+type Props = {
+  mode: "create" | "edit"
+  data: any
+  onClose: () => void
+  onSubmit: () => void
+  onChange: (data: any) => void
+}
+
+export function FamilleForm({
+  mode,
+  data,
+  onClose,
+  onSubmit,
+  onChange,
+}: Props) {
+    return (
+        <form>
+            <div className="mb-4 flex flex-col gap-2 p-2">
+            <Label>Nom</Label>
+            <Input
+                className="h-16"
+                value={data.name || ""}
+                onChange={(e) => onChange({ ...data, name: e.target.value })}
+                placeholder="Nom de la famille"
+            />
+            </div>
+
+            <div className="mt-6 grid grid-cols-2 gap-2">
+            <Button type="button" variant="outline" onClick={onClose}>
+                Annuler
+            </Button>
+            <Button type="button" onClick={onSubmit}>
+                {mode === "create" ? "Créer" : "Modifier"}
+            </Button>
+            </div>
+        </form>
+    )
+}
--- src/app/(main)/famille/page.tsx
+++ src/app/(main)/famille/page.tsx
@@ -1,3 +1,125 @@
+"use client";
+
+import { createColumns, DataTable } from "@/src/shared/components/data-table";
+import { Modal } from "@/src/shared/components/modal";
+import { Spinner } from "@/src/shared/components/spinner";
+import { Button } from "@/src/shared/components/ui/button";
+import { useEffect, useState } from "react";
+import { ClientForm } from "../clients/form/client-form";
+import { ColumnDef } from "@tanstack/react-table";
+import { Badge } from "@/src/shared/components/ui/badge";
+import { FamilleForm } from "./forms/famille-form";
+
 export default function FamillePage() {
-  return <div>Famille Page</div>
+    const [loading, setLoading] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+    const [editOpen, setEditOpen] = useState(false);
+    const [formData, setFormData] = useState<any>({});
+    const [familleSearch, setfamilleSearch] = useState<any[]>([]);
+    const [familles, setFamilles] = useState<any[]>([]);
+
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => setLoading(false), 1000);
+        setFamilles([
+            { id: 1, name: "Famille A" },
+            { id: 2, name: "Famille B" },
+            { id: 3, name: "Famille C" },
+        ]);
+    }, []);
+
+    useEffect(() => {
+        setfamilleSearch(familles);
+    },[familles]);
+
+    const columns: ColumnDef<any>[] = [
+        {
+            accessorKey: "name",
+            header: "Nom",
+        },
+        {
+            header: "Actions",
+            cell: ({ row }) => (
+            <div className="flex gap-2">
+                <Button variant="outline" onClick={() => handleUpdate(row.original)}>
+                Modifier
+                </Button>
+                <Button variant="destructive" onClick={() => handleDelete(row.original)}>
+                Supprimer
+                </Button>
+            </div>
+            ),
+        },
+    ];
+
+    const tableColumns = createColumns({columns});
+
+    const handleSearch = (e: string) => {
+        const filtered = familles.filter((famille) =>
+            famille.name.toLowerCase().includes(e.toLowerCase())
+        );
+        setfamilleSearch(filtered);
+    }
+    const handleCreate = async () => {
+        let newFamilles = [...familles, {id: familles.length + 1, name: formData.name}]
+        setFamilles(newFamilles)
+        setIsOpen(false)
+    }
+    const handleUpdate = async (data: any) => {
+        setFormData(data)
+        setEditOpen(true)
+    }
+    const handleUpdateSubmit = async () => {
+        let updatedFamilles = familles.map(f=>{
+            if(f.id === formData.id){
+                f.name = formData.name
+            }
+        })
+        setFamilles(updatedFamilles)
+        setFormData({})
+        setEditOpen(false)
+    }
+
+    const handleDelete = async (data: any) => {
+        if(!confirm(`Supprimer la famille ${data.name} ?`)) return;
+        let filteredFamilles = familles.filter(f=>f.id !== data.id)
+        setFamilles(filteredFamilles)
+    }
+
+    return(
+        <div className="p-10">
+            <div className="flex justify-between mb-2 p-6 items-center">
+                <h2 className="font-bold text-2xl">Gestion des familles articles</h2>
+                <Button onClick={() => setIsOpen(true)}>Ajouter une famille</Button>
+            </div>
+            
+            {loading && <div className="px-6 flex justify-center mb-4">
+                <Spinner className="size-6" />
+            </div>}
+
+            {<DataTable data={familleSearch} columnsProps={tableColumns} handleSearch={(e)=>handleSearch(e)}/>}
+
+            {/* CREATE */}
+            <Modal open={isOpen} modalTitle="Nouvelle famille" onClose={() => setIsOpen(false)}>
+                <FamilleForm
+                    mode="create"
+                    data={formData}
+                    onChange={setFormData}
+                    onClose={() => setIsOpen(false)}
+                    onSubmit={handleCreate}
+                />
+            </Modal>
+
+            {/* EDIT */}
+            <Modal open={editOpen} modalTitle="Modifier famille" onClose={() => setEditOpen(false)}>
+                <FamilleForm
+                    mode="edit"
+                    data={formData}
+                    onChange={setFormData}
+                    onClose={() => setEditOpen(false)}
+                    onSubmit={handleUpdateSubmit}
+                />
+            </Modal>
+        </div>
+    )
 }
