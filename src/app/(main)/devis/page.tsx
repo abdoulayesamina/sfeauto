ab915076984c99ed7390ae362f0c80891e18688c
Abdoulaye <abdoulaye.cisse@samina.tech>
2026-02-23 12:35:20 +0000

    diminution des elements du devis

--- src/app/(main)/devis/page.tsx
+++ src/app/(main)/devis/page.tsx
@@ -273,7 +273,7 @@ export default function DevisPage() {
         />
       )}
 
-      <Modal open={openApercu} onClose={()=>setOpenApercu(false)} modalTitle="Aperçu du devis">
+      <Modal open={openApercu} onClose={()=>setOpenApercu(false)} modalTitle="">
         <DevisApercu devisId={targetDevisIdForApercu} onClose={()=>setOpenApercu(false)} />
       </Modal>
     </div>
